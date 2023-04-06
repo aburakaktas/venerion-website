b57dc824236222398f462e1f73daf73e0ea23078
Burak Aktas <aburakaktas@gmail.com>
2023-04-06 21:56:09 +0200

    Layout updates - Added color styles - Improved the mobile layout overall - Added "ready to collaborate" section

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,12 @@ module.exports = {
       fontFamily: {
         'sans': ['Inter', 'Helvetica', 'Arial', 'sans-serif'],
       },
+      colors: {
+        primary: '#1A535C',
+        secondary: '#4ECDC4',
+        tertiary: '#F7FFF7',
+        tertiary_bg: '#F1F6F1'
+      }
     },
   },
   plugins: [],
